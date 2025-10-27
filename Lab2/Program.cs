d34eb0950050ea23426bf3f77655ecf3eca5fdb6
Viktoriia5d <vdragun06@gmail.com>
2025-10-27 14:52:41 +0100

    Update Program.cs

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// komentarz
 namespace Programowanie_obiektowe_71445.Laboratorium
 {
     internal class Lab2
