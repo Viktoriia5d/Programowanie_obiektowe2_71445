858db39a4236614e10657e433bc679c1c13a0a60
Viktoriia Drahun <71445@student.wsiz.edu.pl>
2025-12-15 14:42:30 +0100

    wykonano lab6 nowe

---
+++ Lab6/Lab6_nowe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string Imie { get; set; } = "";
+            public string Nazwisko { get; set; } = "";
+            public List<Ocena> Oceny { get; set; } = new();
+        }
+        public class Ocena
+        {
+            public int OcenaId { get; set; }
+            public double Wartosc { get; set; }
+            public string Przedmiot { get; set; } = "";
+            public int StudentId { get; set; }
+        }
+    }
+}
