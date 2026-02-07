61e582defddb6ced5e2bcff6194e8dfd35043af4
Viktoriia5d <vdragun06@gmail.com>
2026-02-07 22:45:20 +0100

    Update Program.cs

--- Lab6/Lab6_nowe/Program.cs
+++ Lab6/Lab6_nowe/Program.cs
@@ -8,26 +8,61 @@ using Microsoft.Data.SqlClient;
 
 namespace Lab6
 {
-    internal class Program
+    public class Student
+{
+    public int StudentId { get; set; }
+    public string Imie { get; set; } = "";
+    public string Nazwisko { get; set; } = "";
+    public List<Ocena> Oceny { get; set; } = new();
+}
+
+public class Ocena
+{
+    public int OcenaId { get; set; }
+    public double Wartosc { get; set; }
+    public string Przedmiot { get; set; } = "";
+    public int StudentId { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
+        string connectionString = "Data Source=10.200.2.28;" +
+                                  "Initial Catalog=studenci_71445;" +
+                                  "Integrated Security=True;" +
+                                  "Encrypt=True;" +
+                                  "TrustServerCertificate=True";
 
-        }
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Połączono z bazą.");
 
-        public class Student
-        {
-            public int StudentId { get; set; }
-            public string Imie { get; set; } = "";
-            public string Nazwisko { get; set; } = "";
-            public List<Ocena> Oceny { get; set; } = new();
+                WyswietlStudentow(connection);
+            }
         }
-        public class Ocena
+        catch (Exception exc)
         {
-            public int OcenaId { get; set; }
-            public double Wartosc { get; set; }
-            public string Przedmiot { get; set; } = "";
-            public int StudentId { get; set; }
+            Console.WriteLine("Wystąpił błąd: " + exc.Message);
         }
     }
+
+    public static void WyswietlStudentow(SqlConnection connection)
+    {
+        string query = "SELECT student_id, imie, nazwisko FROM student";
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["student_id"]}: {reader["imie"]} {reader["nazwisko"]}");
+                }
+            }
+        }
+    }
+}
 }
